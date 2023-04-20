939adb9eded55ec696ed6bf3dc5a88f754acc691
NarhiAkseli <gr265736@gradia.fi>
2023-04-20 17:45:22 +0300

    Add instructions

---
+++ 01_BasicExcercises/Tehtävä 1.3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tehtävä_1._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
