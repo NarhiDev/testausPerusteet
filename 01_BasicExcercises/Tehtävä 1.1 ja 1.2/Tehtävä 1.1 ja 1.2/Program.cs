9d2bd6a216a1d5f86920a4c896d0817da0687733
NarhiAkseli <gr265736@gradia.fi>
2023-04-20 17:42:07 +0300

    Tehtävä 1.1 ja 1.2 done

---
+++ 01_BasicExcercises/Tehtävä 1.1 ja 1.2/Tehtävä 1.1 ja 1.2/Program.cs
@@ -0,0 +1,86 @@
+﻿
+namespace Tehtävä_1._1_ja_1._2
+{
+
+    public class Mathematics
+    {
+        static public void Main(String[] args)
+        {
+            Console.WriteLine("Hello world!");
+        }
+
+        public static int Remainder(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public static double Power(int num)
+        {
+            if (num > 99)
+            {
+                return num;
+            }
+            else
+            {
+                return Math.Pow(num, 2);
+            }
+
+        }
+        public static double Square(int num)
+        {
+            return Math.Sqrt(num);
+        }
+        public static double FindSmallest(List<double> list)
+        {
+            if (list == null || list.Count == 0)
+            {
+                throw new ArgumentException("List cannot be null or empty.");
+            }
+
+            double smallest = list.First();
+            foreach (double number in list)
+            {
+                if (number < smallest)
+                {
+                    smallest = number;
+                }
+            }
+
+            return smallest;
+        }
+        public static int FindMaxValue(List<int> numbers)
+        {
+            if (numbers == null || numbers.Count == 0)
+            {
+                throw new ArgumentException("List cannot be null or empty.");
+            }
+
+            int max = numbers[0];
+            for (int i = 1; i < numbers.Count; i++)
+            {
+                if (numbers[i] > max)
+                {
+                    max = numbers[i];
+                }
+            }
+
+            return max;
+        }
+        public static float CalculateAverage(List<float> numbers)
+        {
+            if (numbers == null || numbers.Count == 0)
+            {
+                throw new ArgumentException("List cannot be null or empty.");
+            }
+
+            float sum = 0;
+            foreach (float number in numbers)
+            {
+                sum += number;
+            }
+
+            return sum / numbers.Count;
+        }
+    }
+}
+
---
+++ 01_BasicExcercises/Tehtävä 1.1 ja 1.2/Unit Tests/UnitTest1.cs
@@ -0,0 +1,247 @@
+namespace Unit_Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Remainder_OnProperValues_ReturnsRemainder()
+        {
+
+            //Arrange
+            int num1 = 2;
+            int num2 = 1;
+            int expectednum = 1;
+
+            //Act
+            int returnednum = Mathematics.Remainder(num1, num2);
+
+            //Assert
+            Assert.AreEqual(expectednum, returnednum);
+        }
+
+        [TestMethod]
+        public void Remainder_IsNotNull_ReturnsRemainder()
+        {
+
+            //Arrange
+            int num1 = 2;
+            int num2 = 1;
+
+            //Act
+            int returnednum = Mathematics.Remainder(num1, num2);
+
+            //Assert
+            Assert.IsNotNull(returnednum);
+        }
+
+        [TestMethod]
+        public void Power_OnProperValues_ReturnsPower()
+        {
+
+            //Arrange
+            int num = 4;
+            int expectednum = 16;
+
+            //Act
+            double returnednum = Mathematics.Power(num);
+
+            //Assert
+            Assert.AreEqual(expectednum, returnednum);
+        }
+        [TestMethod]
+        public void Power_IsTrue_ReturnsPower()
+        {
+
+            //Arrange
+            int num = 4;
+            int expectednum = 16;
+
+            //Act
+            double returnednum = Mathematics.Power(num);
+
+            //Assert
+            Assert.IsTrue(returnednum == expectednum);
+        }
+        public void Square_OnProperValues_ReturnSquare()
+        {
+
+            //Arrange
+            int num = 4;
+            int expectednum = 2;
+
+            //Act
+            double returnednum = Mathematics.Square(num);
+
+            //Assert
+            Assert.AreEqual(expectednum, returnednum);
+        }
+        public void Square_IntOutOfRange_ThrowsException()
+        {
+
+            //Arrange
+            int num = 4;
+            try
+            {
+                //Act
+                double returnednum = Mathematics.Square(num);
+                num += int.MaxValue;
+            }
+
+            catch
+            {
+                //Assert
+                Assert.ThrowsException<ArgumentOutOfRangeException>(() => Mathematics.Square(num));
+            }
+
+        }
+        [TestMethod]
+        public void FindSmallest_GivenListWithOneItem_ReturnsThatItem()
+        {
+            // Arrange
+            List<double> list = new List<double> { 42.0 };
+
+            // Act
+            double result = Mathematics.FindSmallest(list);
+
+            // Assert
+            Assert.AreEqual(42.0, result);
+        }
+
+        [TestMethod]
+        public void FindSmallest_GivenListWithMultipleItems_ReturnsSmallestItem()
+        {
+            // Arrange
+            List<double> list = new List<double> { 3.14, 2.71, 1.41, 0.0, -1.0 };
+
+            // Act
+            double result = Mathematics.FindSmallest(list);
+
+            // Assert
+            Assert.AreEqual(-1.0, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void FindSmallest_GivenEmptyList_ThrowsArgumentException()
+        {
+            // Arrange
+            List<double> list = new List<double>();
+
+            // Act
+            Mathematics.FindSmallest(list);
+
+            // Assert
+            // Expected exception
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void FindSmallest_GivenNullList_ThrowsArgumentException()
+        {
+            // Arrange
+            List<double> list = null;
+
+            // Act
+            Mathematics.FindSmallest(list);
+
+            // Assert
+            // Expected exception
+        }
+        [TestMethod]
+        public void FindMaxValue_ReturnsMaxValue()
+        {
+
+            //Arrange
+            List<int> numbers = new List<int>() { 2, 4, 1, 5, 3 };
+            int expectedMax = 5;
+
+            //Act
+            int returnedMax = Mathematics.FindMaxValue(numbers);
+
+            //Assert
+            Assert.AreEqual(expectedMax, returnedMax);
+        }
+
+        [TestMethod]
+        public void FindMaxValue_IsNotNull_ReturnsMaxValue()
+        {
+            ;
+
+            //Arrange
+            List<int> numbers = new List<int>() { 2, 4, 1, 5, 3 };
+
+            //Act
+            int returnedMax = Mathematics.FindMaxValue(numbers);
+
+            //Assert
+            Assert.IsNotNull(returnedMax);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void FindMaxValue_ListIsEmpty_ThrowsArgumentException()
+        {
+
+            //Arrange
+            List<int> numbers = new List<int>();
+
+            //Act
+            int returnedMax = Mathematics.FindMaxValue(numbers);
+
+            //Assert
+            // Expected exception
+
+        }
+        [TestMethod]
+
+        [ExpectedException(typeof(ArgumentException))]
+        public void FindMaxValue_ListIsNull_ThrowsArgumentException()
+        {
+
+            //Arrange
+            List<int> numbers = null;
+
+            //Act
+            int returnedMax = Mathematics.FindMaxValue(numbers);
+
+            //Assert
+            // Expected exception
+        }
+
+        [TestMethod]
+        public void CalculateAverage_ReturnsCorrectValue()
+        {
+            // Arrange
+            List<float> numbers = new List<float> { 1.0f, 2.0f, 3.0f, 4.0f };
+            float expected = 2.5f;
+
+            // Act
+            float result = Mathematics.CalculateAverage(numbers);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CalculateAverage_ListIsNull_ThrowsArgumentException()
+        {
+            // Arrange
+            List<float> numbers = null;
+
+            // Act
+            Mathematics.CalculateAverage(numbers);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CalculateAverage_ListIsEmpty_ThrowsArgumentException()
+        {
+            // Arrange
+            List<float> numbers = new List<float>();
+
+            // Act
+            Mathematics.CalculateAverage(numbers);
+        }
+    }
+}
